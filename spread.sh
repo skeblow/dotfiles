7545d0d41b3c7efe3c8da3a03350e978985cce6c
skeblow <skeblow@gmail.com>
2022-10-10 13:14:02 +0200

    delete useless stuff

--- spread.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp vim/init.vim ~/.config/nvim/init.vim
-cp -r vim/colors ~/.config/nvim/colors
-cp -r bin ~/.bin
-cp skhdrc ~/.skhdrc
-sh yabairc
-cp yabairc ~/.yabairc
-cp zshrc ~/.zshrc
--- ubers/bar/src/index.jsx
+++
@@ -1,9 +0,0 @@
-export {default as Mem} from './mem.jsx';
-export {default as Battery } from './battery.jsx';
-export {default as Cpu } from './cpu.jsx';
-export {default as Playing } from './playing.jsx';
-export {default as Time } from './time.jsx';
-export {default as Workspaces } from './workspaces.jsx';
-export {default as FocusedApp} from './focusedApp.jsx';
-export {default as Hdd} from './hdd.jsx';
-
